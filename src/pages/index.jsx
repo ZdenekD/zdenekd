8e6314f6dadf30f06a3e7c200fef3aa0d258c56a
zdenekd <ditezdenek@gmail.com>
2020-01-24 13:22:32 +0100

    Add some initial styles (reset, normalize, variables)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import styles from './index.css';
 import Heading from '../components/heading';
 
-const Homepage = () => <Heading title="Homepage" />;
+const Homepage = () => (
+    <main className={styles.default}>
+        <Heading title="Heading" />
+    </main>
+);
 
 export default Homepage;
