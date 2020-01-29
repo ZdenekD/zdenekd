8e3a8eb8596126324424d6a076543f07d8c65279
zdenekd <ditezdenek@gmail.com>
2020-01-29 16:46:12 +0100

    Prepare heading and navigation

--- src/components/layout/main/main.jsx
+++ src/components/layout/main/main.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './main.css';
 import Header from '../header';
 import Content from '../content';
 import Aside from '../aside';
+import DATA from '../../../data/pages';
 
-const Main = () => (
-    <main className={styles.default} data-test="component-main">
-        <section className={styles.section}>
-            <Header title="ZdenekD" />
-            <Content />
-            <Aside />
-        </section>
-    </main>
-);
+const Main = ({site}) => {
+    const {title} = DATA[site];
+
+    return (
+        <main className={styles.default} data-test="component-main">
+            <section className={`${styles.section} ${styles[site]}`}>
+                <Header title={`${title} | ZdenekD`} />
+                <Content />
+                <Aside />
+            </section>
+        </main>
+    );
+};
+
+Main.propTypes = {site: PropTypes.string.isRequired};
 
 export default Main;
--- src/components/layout/main/main.spec.js
+++ src/components/layout/main/main.spec.js
@@ -1,9 +1,11 @@
 import {factory, findComponent} from '../../../../__tests__/utils/helpers';
 import Main from './index';
+import DATA from '../../../data/pages';
 
 describe('Main', () => {
     it('renders wihout error', () => {
-        const wrapper = factory(Main);
+        const site = Object.keys(DATA)[0];
+        const wrapper = factory(Main, {site});
         const component = findComponent(wrapper, 'component-main');
 
         expect(component.length).toBe(1);
