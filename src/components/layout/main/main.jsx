01fb7892dbe54b45c707d2ac0606d38cd1e6c032
zdenekd <ditezdenek@gmail.com>
2020-01-31 16:22:45 +0100

    Temporary styles for master branch

--- src/components/layout/main/main.jsx
+++ src/components/layout/main/main.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 import styles from './main.css';
 import Header from '../header';
 import Content from '../content';
 import DATA from '../../../data/pages';
 
-const Aside = dynamic(() => import('../aside'));
-const Main = ({site}) => {
+// const Aside = dynamic(() => import('../aside'));
+const Main = ({site, children}) => {
     const {title} = DATA[site];
 
     return (
-        <main className={styles.default} data-test="component-main">
-            <section className={`${styles.section} ${styles[site]}`}>
-                <Header title={`${title} | ZdenekD`} />
-                <Content />
-                <Aside />
-            </section>
-        </main>
+        <>
+            <main className={styles.default} data-test="component-main">
+                <section className={`${styles.section} ${styles[site]}`}>
+                    <Header title={`${title} | ZdenekD`} />
+                    <Content />
+                    {children}
+                    {/* <Aside /> */}
+                </section>
+            </main>
+        </>
     );
 };
 
-Main.propTypes = {site: PropTypes.string.isRequired};
+Main.propTypes = {
+    site: PropTypes.string.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element,
+        PropTypes.node,
+        PropTypes.number,
+    ]),
+};
 
 export default Main;
