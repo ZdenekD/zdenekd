5121b5a3ea3976f1d86c56aab6acd1ce448e8889
zdenekd <ditezdenek@gmail.com>
2020-02-27 20:07:36 +0100

    Edit test helper

--- src/__test__/utils/helpers.js
+++ src/__test__/utils/helpers.js
@@ -15,15 +15,10 @@ export const getStore = state => applyMiddleware(...middlewares)(createStore)(re
  * @function setup
  * @param {ReactComponent} Component - React component to render with shallow
  * @param {Object} props - Component props specific to this setup
- * @param {Object} state - Initial state for setup
  * @returns {ShallowWrapper}
  */
-export const factory = (Component, props = {}, state = null) => {
+export const factory = (Component, props = {}) => {
     const wrapper = shallow(<Component {...props} />);
-
-    if (state) {
-        wrapper.setState(state);
-    }
 
     return wrapper;
 };
