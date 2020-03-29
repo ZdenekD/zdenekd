2e166b3ef2122f9562077b474b802ee4e8f23401
zdenekd <ditezdenek@gmail.com>
2020-03-29 21:03:42 +0200

    Edit postcss stylelint for development

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     modules: true,
     map: process.env.NODE_ENV === 'development',
     plugins: {
-        stylelint: process.env.NODE_ENV === 'development' ? {} : false,
+        stylelint: process.env.NODE_ENV === 'development' ? {} : undefined,
         'postcss-import': {},
         'postcss-nested': {},
         'postcss-preset-env': {
