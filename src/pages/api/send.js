494a09591e2a24c2fdbf6c257e10563964a4f91c
zdenekd <ditezdenek@gmail.com>
2020-03-30 22:26:38 +0200

    Edit send api

--- src/pages/api/send.js
+++ src/pages/api/send.js
@@ -1,30 +1,30 @@
 const sendgrid = require('@sendgrid/mail');
 
 export default async function(req, res) {
-    sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+    const {method} = req;
 
-    const {name, email, message} = req.body;
+    if (method !== 'POST') {
+        res.status(405).end(`Method ${method} not allowed.`);
+    } else {
+        sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-    const content = {
-        to: 'ditezdenek@pm.me',
-        from: 'contact@zdenekd.cz',
-        subject: 'Contact form',
-        text: message,
-        html: `
-            <p>
-                Zpráva od: <strong>${name}</strong> - <strong>${email}</strong>
-            </p>
-            <p>
-                ${message}
-            </p>
-        `,
-    };
+        const {name, email, message} = req.body;
 
-    try {
-        await sendgrid.send(content);
+        const content = {
+            to: 'ditezdenek@pm.me',
+            from: 'contact@zdenekd.cz',
+            subject: 'Contact form',
+            text: message,
+            // eslint-disable-next-line prefer-template
+            html: '<p>Zpráva od: <strong>' + name + '</strong> - <strong>' + email + '</strong></p><p>' + message + '</p>',
+        };
 
-        res.status(200).send('Zpráva byla úspěšně odeslána.');
-    } catch (error) {
-        res.status(400).send('Něco se bohužel po@#$&lo.');
+        try {
+            await sendgrid.send(content);
+
+            res.status(200).send('Zpráva byla úspěšně odeslána.');
+        } catch (error) {
+            res.status(400).send('Něco se bohužel po@#$&lo.');
+        }
     }
 }
