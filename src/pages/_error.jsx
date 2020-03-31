8cd8ea99c2f6e068e7897162ad058c31387dcf02
zdenekd <ditezdenek@gmail.com>
2020-03-31 14:35:23 +0200

    Add error page animation (404)

--- src/pages/_error.jsx
+++ src/pages/_error.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 import './_index.css';
 import Main from '../components/layout/main';
+import styles from './_error.css';
+import config from '../data/config';
 
-const Error = ({statusCode = 404}) => (
-    <Main data-test="component-error">
-        <h2 data-test="component-status">{statusCode}</h2>
-        <p>
-            Někde se stala chyba. Na opravě se pracuje.
-        </p>
-    </Main>
+const Error = () => (
+    <>
+        <Head>
+            <script async src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+        </Head>
+        <Main data-test="component-error">
+            <lottie-player loop autoplay src={`${config.cloudfront}/error.json`} speed="1" class={styles.animation}></lottie-player>
+        </Main>
+    </>
 );
 
 Error.propTypes = {statusCode: PropTypes.number};
