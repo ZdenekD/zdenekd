0cef6f3a3cf24956ef540995d6f598f267519629
zdenekd <ditezdenek@gmail.com>
2020-04-02 22:21:19 +0200

    Update pages description

--- src/data/pages.js
+++ src/data/pages.js
@@ -3,19 +3,19 @@ const data = {
         link: '/',
         title: 'Úvodní stránka',
         heading: 'ZdenekD',
-        description: 'Lorem pivsum | Homepage',
+        description: 'Tvorba webů, ruční práce, klávesový koncert | Homepage',
     },
     projects: {
         link: '/projects',
         title: 'Projekty',
         heading: 'Projekty',
-        description: 'Lorem pivsum | Projects',
+        description: 'Projekty, spolupráce a jiné radosti | Projects',
     },
     contact: {
         link: '/contact',
         title: 'Kontakt',
         heading: 'Kontakt',
-        description: 'Lorem pivsum | Contact',
+        description: 'Ozvěte se a vymyslíme řešení | Contact',
     },
 };
 
