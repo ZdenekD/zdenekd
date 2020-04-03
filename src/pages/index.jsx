cb822a986cb61072641e34f9c054de41020153da
zdenekd <ditezdenek@gmail.com>
2020-04-03 22:03:14 +0200

    Update homepage test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,8 +8,8 @@ const Homepage = () => (
         <h1 data-test="component-heading">{data.homepage.heading}</h1>
         <article className={styles.article}>
             <p className={`${styles.paragraph} animated-block`}>
-                Profesionální <strong data-tooltip="zírání">hře</strong> na&nbsp;klávesnici se&nbsp;věnuji několik let.<br />
-                Převedu všechny vaše přání do kódu, kterému rozumí lidé i počítače.
+                Profesionální hře na&nbsp;klávesnici se&nbsp;věnuji několik let.<br />
+                Převedu vaše přání do kódu, kterému rozumí lidé i počítače.
             </p>
             <p className={`${styles.paragraph} _align-right animated-block`}>
                 <span>Tvořím s <strong data-tooltip="kávou">láskou</strong> a <strong data-tooltip="lezením">nadšením</strong>.</span><br />
