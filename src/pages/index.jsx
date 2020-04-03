921ca4cd8caf6ea7b451e43131d01709ffb9731a
zdenekd <ditezdenek@gmail.com>
2020-04-03 22:26:24 +0200

    Update homepage text

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,7 +9,7 @@ const Homepage = () => (
         <article className={styles.article}>
             <p className={`${styles.paragraph} animated-block`}>
                 Profesionální hře na&nbsp;klávesnici se&nbsp;věnuji několik let.<br />
-                Převedu vaše přání do kódu, kterému rozumí lidé i počítače.
+                Pomohu vám převést přání do kódu, kterému rozumí lidé i počítače.
             </p>
             <p className={`${styles.paragraph} _align-right animated-block`}>
                 <span>Tvořím s <strong data-tooltip="kávou">láskou</strong> a <strong data-tooltip="lezením">nadšením</strong>.</span><br />
