3dd87cfb7107146b5330f78e4071a15ef952b5e6
zdenekd <ditezdenek@pm.me>
2021-02-25 15:02:14 +0100

    Edit cursor component

--- src/components/cursor/cursor.tsx
+++ src/components/cursor/cursor.tsx
@@ -18,6 +18,8 @@ const Cursor: React.FC = () => {
         outer.style.transform = `translate3d(${coordX}px, ${coordY}px, 0)`;
         outer.style.setProperty('--cursor-width', '30px');
         outer.style.setProperty('--cursor-height', '30px');
+        outer.style.setProperty('--cursor-radius', '50%');
+        outer.style.border = '1px solid';
         inner.style.transform = `matrix(1, 0, 0, 1, ${coordX}, ${coordY})`;
     };
     const handleIsStuck: IHandler = (coordX, coordY, inner, outer) => {
@@ -26,6 +28,8 @@ const Cursor: React.FC = () => {
         outer.style.transform = `translate3d(${left + 10}px, ${top + 10}px, 0)`;
         outer.style.setProperty('--cursor-width', `${width + 10}px`);
         outer.style.setProperty('--cursor-height', `${height + 10}px`);
+        outer.style.setProperty('--cursor-radius', '8px');
+        outer.style.border = '2px dotted';
         inner.style.transform = `matrix(1, 0, 0, 1, ${coordX}, ${coordY})`;
     };
     const handleCursor = (coordX: number, coordY: number) => {
@@ -44,10 +48,7 @@ const Cursor: React.FC = () => {
 
     return (
         <>
-            <i
-                ref={outerCursorRef}
-                className={`${styles.outerCursor} ${state.cursor.isStuck ? styles.outerCursorStuck : ''}`} data-test="component-outer-cursor"
-            />
+            <i ref={outerCursorRef} className={styles.outerCursor} data-test="component-outer-cursor" />
             <i
                 ref={innerCursorRef}
                 className={`${styles.innerCursor} ${state.cursor.isStuck ? styles.innerCursorStuck : ''}`}
