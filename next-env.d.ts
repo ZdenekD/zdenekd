c00dca88562d168c2b3adcc688952389570972d5
zdenekd <ditezdenek@pm.me>
2021-03-03 21:24:46 +0100

    Edit next-env definitions file

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,12 +3,22 @@ declare module '*.svg' {
     export default content;
 }
 
+declare module '*.avif' {
+    const content: string;
+    export default content;
+  }
+
 declare module '*.png' {
     const content: string;
     export default content;
 }
 
+declare module '*.webp' {
+    const content: string;
+    export default content;
+}
+
 declare module '*.css' {
-    const content: {[key: string]: string};
+    const content: {readonly [key: string]: string};
     export default content;
 }
