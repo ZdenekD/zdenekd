20fd86d4a88d615bd1baf17c1fab868d0739c95c
zdenekd <ditezdenek@pm.me>
2021-03-05 18:01:03 +0100

    Update references for mobile

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
 import Menu from '../menu';
 import Logo from '../logo';
 import {useStateValue} from '../../state';
 import styles from './header.css';
 
 const Header: React.FC = () => {
-    const router = useRouter();
     const {state} = useStateValue();
-    const isHomepage = router.route === '/';
 
     return (
         <header className={styles.header} data-test="component-header">
-            {isHomepage ? (
-                <Logo />
-            ) : (
-                <Link passHref href="/">
-                    <a
-                        href="/"
-                        className={`${styles.link} ${state.animation.isAsideAnimated ? styles.disabled : ''}`}
-                        aria-label="Odkaz na domovskou stránku"
-                    >
-                        <Logo />
-                    </a>
-                </Link>
-            )}
+            <Link passHref href="/">
+                <a
+                    href="/"
+                    className={`${styles.link} ${state.animation.isAsideAnimated ? styles.disabled : ''}`}
+                    aria-label="Odkaz na domovskou stránku"
+                >
+                    <Logo />
+                </a>
+            </Link>
             <Menu />
         </header>
     );
--- src/components/project/browser/browser.tsx
+++ src/components/project/browser/browser.tsx
@@ -3,7 +3,10 @@ import Controls from './controls';
 import Video from './video';
 import Addressbar from './addressbar';
 import Target from './target';
+import Picture from '../../../UI/picture';
+import config from '../../../data/config';
 import {IProject} from '../../../data/projects';
+import useWindowSize from '../../../hooks/useWindowSize';
 import styles from './browser.css';
 
 interface IBrowser {
@@ -20,30 +23,43 @@ const Browser = React.forwardRef<HTMLElement, IBrowser>(({
     isLast,
     handlePrev = undefined,
     handleNext = undefined,
-}, ref) => (
-    <section
-        ref={ref}
-        className={styles.browser}
-        data-test="component-browser"
-    >
-        <header className={styles.header}>
-            <span className={styles.controls}>
-                <i className={styles.control} />
-                <i className={styles.control} />
-                <i className={styles.control} />
-            </span>
-            <Controls
-                isFirst={isFirst}
-                isLast={isLast}
-                handlePrev={handlePrev}
-                handleNext={handleNext}
-            />
-            <Addressbar url={project.url} />
-            <Target url={project.url} />
-        </header>
-        <Video project={project} />
-    </section>
-));
+}, ref) => {
+    const {width} = useWindowSize();
+
+    return (
+        <section
+            ref={ref}
+            className={styles.browser}
+            data-test="component-browser"
+        >
+            <header className={styles.header}>
+                <span className={styles.controls}>
+                    <i className={styles.control} />
+                    <i className={styles.control} />
+                    <i className={styles.control} />
+                </span>
+                <Controls
+                    isFirst={isFirst}
+                    isLast={isLast}
+                    handlePrev={handlePrev}
+                    handleNext={handleNext}
+                />
+                <Addressbar url={project.url} />
+                <Target url={project.url} />
+            </header>
+            {width > 600 ? (
+                <Video project={project} />
+            ) : (
+                <Picture
+                    src={`${config.cloudfront}/image/mobile_${project.id}`}
+                    width={480}
+                    height={270}
+                />
+            )}
+
+        </section>
+    );
+});
 
 Browser.displayName = 'Browser';
 
