13ce545354abe3b300bc3bea298fdfefeeabc77c
zdenekd <ditezdenek@pm.me>
2021-05-06 18:01:27 +0200

    Remove @tabler/icon dependency (huge package)

--- src/UI/icon/icon.tsx
+++ src/UI/icon/icon.tsx
@@ -1,5 +1,3 @@
-import {IconBrandGithub, IconMessage} from '@tabler/icons';
-
 export enum IconsEnum {
     message = 'message',
     brandGithub = 'brandGithub'
@@ -13,17 +11,37 @@ interface IIcon {
 const Icon: React.FC<IIcon> = ({variant, size = 28}) => (
     <>
         {variant === IconsEnum.brandGithub && (
-            <IconBrandGithub
-                size={size}
-                stroke={1}
-            />
+            <svg
+                width={size}
+                height={size}
+                viewBox={`0 0 ${size - 4} ${size - 4}`}
+                strokeWidth="1"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"></path>
+            </svg>
         )}
 
         {variant === IconsEnum.message && (
-            <IconMessage
-                size={size}
-                stroke={1}
-            />
+            <svg
+                width={size}
+                height={size}
+                viewBox={`0 0 ${size - 4} ${size - 4}`}
+                strokeWidth="1"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+            >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 21v-13a3 3 0 0 1 3 -3h10a3 3 0 0 1 3 3v6a3 3 0 0 1 -3 3h-9l-4 4"></path>
+                <line x1="8" y1="9" x2="16" y2="9"></line>
+                <line x1="8" y1="13" x2="14" y2="13"></line>
+            </svg>
         )}
     </>
 );
