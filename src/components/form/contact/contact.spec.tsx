bc7475361af5f746201dacbf4a6df2213632f45a
zdenekd <ditezdenek@pm.me>
2021-11-07 17:30:52 +0100

    Remove unused contact form tests

--- src/components/form/contact/contact.spec.tsx
+++ src/components/form/contact/contact.spec.tsx
@@ -4,15 +4,6 @@ import findComponent from '@/__test__/utils/helpers';
 import '@/__test__/mocks/nextRouterMock';
 import Contact from '.';
 
-const handleSubmit = jest.fn();
-const useForm = () => ({
-    register: jest.fn(),
-    errors: [],
-    handleSubmit,
-});
-
-jest.mock('react-hook-form', () => ({useForm}));
-
 describe('Form/Contact', () => {
     let wrapper: ShallowWrapper;
 
@@ -24,22 +15,5 @@ describe('Form/Contact', () => {
         const component = findComponent(wrapper, 'component-contact');
 
         expect(component.exists()).toBe(true);
-    });
-
-    it('shows loader if form is in disabled state', () => {
-        React.useState = jest.fn(() => [true, jest.fn()]);
-
-        const container = shallow(<Contact />);
-        const component = findComponent(container, 'component-contact-loader');
-
-        expect(component.exists()).toBe(true);
-    });
-
-    it('runs handleSubmit on form submit', () => {
-        const component = findComponent(wrapper, 'component-contact');
-
-        component.find('Button').simulate('click');
-
-        expect(handleSubmit).toHaveBeenCalled();
     });
 });
