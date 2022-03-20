9b6be94c7afcfbf9d36e2e937edc201644b8365a
zdenekd <ditezdenek@pm.me>
2022-03-20 18:34:39 +0100

    Update app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,21 @@
 import {AnimatePresence} from 'framer-motion';
-import App, {AppProps} from 'next/app';
+import {AppProps} from 'next/app';
+import React from 'react';
 import {Provider} from '../store';
 import '@/assets/styles/index.css';
 
-class Application extends App {
-    // eslint-disable-next-line class-methods-use-this
-    componentDidMount(): void {
+const App = ({Component, pageProps, router}: AppProps) => {
+    React.useEffect(() => {
         console.info('%cHello Underworld! Authorship is attributed to %c01011010 01100100 01100101 01101110 01100101 01101011 01000100. %cYou can try this >> https://bit.ly/31QKTyy', 'color: #9cd9f2', 'color: #c8e49b', 'color: #9cd9f2');
-    }
+    }, []);
 
-    render(): React.ReactElement {
-        const {Component, pageProps, router}: AppProps = this.props;
+    return (
+        <Provider>
+            <AnimatePresence exitBeforeEnter>
+                <Component {...pageProps} key={router.route} />
+            </AnimatePresence>
+        </Provider>
+    );
+};
 
-        return (
-            <Provider>
-                <AnimatePresence exitBeforeEnter>
-                    <Component {...pageProps} key={router.route} />
-                </AnimatePresence>
-            </Provider>
-        );
-    }
-}
-
-export default Application;
+export default App;
