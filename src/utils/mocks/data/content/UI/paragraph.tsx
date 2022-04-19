fc425000f661ae95d5e70123a324f0465b7e4df1
zdenekd <ditezdenek@pm.me>
2022-04-19 17:06:08 +0200

    Add mocks data

---
+++ src/utils/mocks/data/content/UI/paragraph.tsx
@@ -0,0 +1,7 @@
+const MockDataParagraph = `Bacon ipsum dolor amet cupim sirloin andouille, pork belly porchetta ham hock hamburger
+sausage buffalo swine. Short ribs venison ham hock, fatback shoulder shankle kielbasa tail brisket drumstick cupim
+hamburger short loin sausage filet mignon. Chuck kevin pig, capicola leberkas turducken chislic pork belly meatball
+swine ham biltong. T-bone drumstick tail corned beef prosciutto bresaola. Flank pork loin boudin, drumstick corned beef
+tenderloin alcatra doner chicken swine.`;
+
+export default MockDataParagraph;
